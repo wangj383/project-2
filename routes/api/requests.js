c5caf38a98f9887dfc4921040f062418b42c23f7
Yvon Wang <yvonwang@Yvons-MacBook.local>
2020-03-26 18:03:38 -0400

    herokuuu

--- routes/api/requests.js
+++ routes/api/requests.js
@@ -4,7 +4,6 @@ const requestsCtrl = require('../../controllers/api/requests')
 
 router.get('/',requestsCtrl.index)
 router.get('/:id',requestsCtrl.show)
-// router.post('/', requestsCtrl.create)
 router.post('/', requestsCtrl.create)
 router.put('/:id',requestsCtrl.update)
 router.put('/:id',requestsCtrl.cancelRequest)
